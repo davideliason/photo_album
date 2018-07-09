34c193581971843e73fea2137f29f42d3df52a32
David Eliason <david@davethemaker.com>
2018-07-09 14:56:02 -0700

    successfully return the list of the two album names using fs module readdir

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 const fs = require('fs');
 
+function loadAlbumList(cb) {
+    fs.readdir("albums", (err, files) => {
+        if (err) {
+            cb(error);
+            return;
+        }
+        cb(null, files);
+    })
+}
+
 function handle_requests(req, res) {
     console.log(`method: ${req.method} and url: ${req.url}`);
-    loadAlbumList(cb);
+
+    loadAlbumList((err, data) => {
+        if (err) {
+            console.log("error");
+            return;
+        }
+        console.log(`Number of album groups: ${data}`);
+    });
+
     res.writeHead(200, {
         "Content-Type": "application/json"
     });
@@ -13,15 +31,7 @@ function handle_requests(req, res) {
     }) + "\n");
 }
 
-function loadAlbumList(cb) {
-    fs.readdir("albums", (err, files) => {
-        if (err) {
-            cb(error, null);
-            return;
-        }
-        cb(null, files);
-    })
-}
+
 
 function cb(error, data) {
     if (error) {
