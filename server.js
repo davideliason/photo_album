b2efd8df53c662168fce3555fe0ce4a1d9d9bf5c
David Eliason <david@davethemaker.com>
2018-07-09 14:45:51 -0700

    create a simple callback function to handle error and data upon album retrieval

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
+const fs = require('fs');
 
 function handle_requests(req, res) {
     console.log(`method: ${req.method} and url: ${req.url}`);
+    loadAlbumList(cb);
     res.writeHead(200, {
         "Content-Type": "application/json"
     });
     res.end(JSON.stringify({
         error: null
     }) + "\n");
+}
+
+function loadAlbumList(cb) {
+    fs.readdir("albums", (err, files) => {
+        if (err) {
+            cb(error, null);
+            return;
+        }
+        cb(null, files);
+    })
+}
+
+function cb(error, data) {
+    if (error) {
+        console.log("error");
+    }
+    else {
+        console.log(data.length);
+    }
 }
 
 var server = http.createServer(handle_requests);
